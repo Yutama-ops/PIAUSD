fa39ad2779f7b92542b7fd445b5ff8d9b76a4fbe
TAM <tamcrush@yahoo.com>
2018-02-13 22:16:05 +1100

    cek

--- nav/aboutUS.php
+++ nav/aboutUS.php
@@ -1,4 +1,5 @@
 <!-- About us Modals-->
+
 <div class="portfolio-modal modal fade" id="aboutusModal1" tabindex="-1" role="dialog" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -7,65 +8,31 @@
           <div class="rl"></div>
         </div>
       </div>
+
       <div class="container">
         <div class="row">
           <div class="col-lg-12 text-center">
+            <section id="aboutUs">
             <h2 class="section-heading text-uppercase">About us</h2>
-            <h3 class="section-subheading text-muted">Shaping the future</h3>
+            <h3 class="section-subheading text-muted">Who We Are</h3>
+            <h7 class="section-subheading text-muted">The Indonesian Students Association at the University of Sydney (PPIA USYD) is an organisation which aims to provide a common platform to students who are of Indonesian background, or whom have an interest in Indonesian culture, by providing support through social, cultural and educational events. PPIA USYD organises a wide range of regular sporting, cultural and social events throughout the year. We additionally regularly work with major organisations to help host educational and career seminars in order to provide our members with a strong foundation of employability once they graduate.
+            </h7> <br/><br/><br/><br/><br/><br/><br/>
+            <h3 class="section-subheading text-muted">Our Goal</h3>
+            <h7 class="section-subheading text-muted">Our ultimate goal is to make the university experience of our members as enjoyable as possible through the camaraderie experienced within the sharing of our culture, and also through the guidance of our national motto "Bhinneka Tunggal Ika" - Unity in Diversity.
+            </h7> <br/><br/><br/><br/><br/><br/><br/>
+            <section/>
+            <section id="history">
+            <h2 class="section-heading text-uppercase">HISTORY</h2>
+            <h7 class="section-subheading text-muted">Perhimpunan Pelajar Indonesia Australia (PPIA) adalah organisasi mahasiswa yang tebentuk di Canberra pada tahun 1981. Sejak saat itu, PPIA telah berkembang ke hampir seluruh negara bagian Australia. Hingga saat ini, lebih kurang terdapat 30 ranting PPIA tersebar di Australia. PPIA cabang New South Wales pada contohnya memiliki 7 ranting PPIA diantaranya PPIA USyd, PPIA Macquarie, PPIA UTS, PPIA Newcastle, PPIA UWS, PPIA Wollongong dan juga PPIA UNSW.
+            </h7> <br/><br/><br/><br/><br/><br/><br/>
+            <h7 class="section-subheading text-muted">PPIA usyd sendiri telah berdiri lama, perjuangan oleh masing-masing ketua dan komitenya telah membawa perubahan untuk PPIA usyd sendiri. Saat ini, PPIA usyd telah tergabung menjadi anggota Student Clubs (USU) di Usyd. Hingga saat ini, PPIA Usyd telah memiliki kira-kira 400 anggota yang masih melanjutkan studi di Usyd maupun di universitas lainnya di NSW.
+            </h7> <br/><br/><br/><br/><br/><br/><br/>
+            <section/>
+
+
           </div>
         </div>
-        <div class="row">
-          <div class="col-lg-12">
-            <ul class="timeline">
-              <li>
-                <div class="timeline-image">
-                  <img class="rounded-circle img-fluid" src="img/about/one.jpg" alt="">
-                </div>
-                <div class="timeline-panel">
-                  <div class="timeline-heading">
-                    <h4 class="subheading">Digital Strategy and Planning</h4>
-                  </div>
-                  <div class="timeline-body">
-                    <p class="text-muted">We strive to provide solution and insights to our clients, to grow their business and consumers through our creative ideas.</p>
-                  </div>
-                </div>
-              </li>
-              <li class="timeline-inverted">
-                <div class="timeline-image">
-                  <img class="rounded-circle img-fluid" src="img/about/two.jpg" alt="">
-                </div>
-                <div class="timeline-panel">
-                  <div class="timeline-heading">
-                    <h4 class="subheading">Digital Execution & Social Media</h4>
-                  </div>
-                  <div class="timeline-body">
-                    <p class="text-muted">Create outstanding solutions and user experiences through multiple platform, driving engagement and loyalty.</p>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="timeline-image">
-                  <img class="rounded-circle img-fluid" src="img/about/three.jpg" alt="">
-                </div>
-                <div class="timeline-panel">
-                  <div class="timeline-heading">
-                    <h4 class="subheading">Visual Communication Design</h4>
-                  </div>
-                  <div class="timeline-body">
-                    <p class="text-muted">Using thorough analysis and planning, we design and develop to maximize performance of each aspect.</p>
-                  </div>
-                </div>
-              </li>
-              <li class="timeline-inverted">
-                <div class="timeline-image">
-                  <h4>Be Part
-                    <br>Of Our
-                    <br>Story!</h4>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
+
       </div>
     </div>
   </div>
