c26a195082cb1390ec14a852a1e79cc9e78a9a5d
Geraldi Gunawan <geraldigunawan23@gmail.com>
2018-02-24 22:25:27 +1100

    Finished "Events" tab
    - Completed CSS and JS for "Our events" tab.
    - Completed CSS media for screen smaller than 1200px
    -Completed content provided by business

--- js/fade_in_fade_out.js
+++ js/fade_in_fade_out.js
@@ -1,7 +1,8 @@
 function show_event_decription(id){
   var $selected_ID = $('#event_' + id);
   var event_count = 0;
-  for(a = 1; a<=8; a++){
+  var total_events = 13;
+  for(a = 0; a<=total_events; a++){
     if ($('#event_' + a).is(":not(':hidden')")) {
       //if there is at least one ID visible
       event_count += 1;
@@ -10,27 +11,32 @@ function show_event_decription(id){
   //If the selected ID is visible and it's the only one visible, fade the ID out
     if ($selected_ID.is(":not(':hidden')") & event_count == 1) {
         $selected_ID.fadeOut();
+        $('#event_' + id + "_date").fadeOut("slow");
         $('#event_' + id + "_desc").fadeOut("slow");
     }
   //If the selected ID is visible and it's not the only one visible, fade all of them out
     if ($selected_ID.is(":not(':hidden')") & event_count > 1){
-      for(b = 1; b<=8; b++){
+      for(b = 0; b<=total_events; b++){
         $('#event_' + b).fadeOut();
+        $('#event_' + b + "_date").fadeOut("slow");
         $('#event_' + b + "_desc").fadeOut("slow");
       }
     }
   //If the selected ID is hidden and no other event are visible, fade the ID in
       if ($selected_ID.is(':hidden') & event_count == 0) {
           $selected_ID.fadeIn();
+          $('#event_' + id + "_date").fadeIn("slow");
           $('#event_' + id + "_desc").fadeIn("slow");
       }
   //If the selected ID is hidden and there are other events visible, fade the ID in, fade the other ID out
       if ($selected_ID.is(':hidden') & event_count > 0) {
           $selected_ID.fadeIn();
+          $('#event_' + id + "_date").fadeIn("slow");
           $('#event_' + id + "_desc").fadeIn("slow");
-          for(c = 1; c<=8; c++){
+          for(c = 0; c<=total_events; c++){
             if(c != id){
               $('#event_' + c).fadeOut();
+              $('#event_' + c + "_date").fadeOut("slow");
               $('#event_' + c + "_desc").fadeOut("slow");
             }
           }
@@ -44,9 +50,9 @@ function moveCar(id){
   console.log( "carpositionleft: " + carpositionleft);
 
   <!--get clicked button position-->
-  var p = $( '#week_' + id );
+  var p = $( '#event_no_' + id );
   var pos = p.position();
-  var clickedposleft= pos.left;
+  var clickedposleft= pos.left - 35;
   console.log( "clickedposleft: " + clickedposleft);
 
   var elem = document.getElementById("animate");
--- nav/event.php
+++ nav/event.php
@@ -17,53 +17,208 @@
         <div id="animate"></div>
         </div>
 
+        <div class = "weekparagraph">
+          <span class="weekfirsthalf">
+            <p id="event_no_0">O-week</p>
+            <p id="event_no_1">week 1</p>
+            <p id="event_no_2">week 2</p>
+            <p id="event_no_4">week 4</p>
+          </span>
+          <span class="weekbreak">
+            <p id="event_no_5">BREAK!</p>
+          </span>
+          <span class="weeksecondhalf">
+            <p id="event_no_7">week 6</p>
+            <p id="event_no_9">week 8</p>
+            <p id="event_no_11">week 10</p>
+            <p id="event_no_13">week 12</p>
+          </span>
+        </div>
+
         <div class="progress_bar">
-          <p id="week_1">Week 1</p>
-          <p id="week_2">Week 2</p>
-          <p id="week_3">Week 3</p>
-          <p id="week_4">Week 4</p>
-          <p id="week_5">Week 5</p>
-          <p id="week_6">Week 6</p>
-          <p id="week_7">Week 7</p>
-          <p id="week_8">Week 8</p>
           <img src="img/events/progress.png">
         </div>
           <div class="button">
-            <img onClick="show_event_decription(this.id);moveCar(this.id);" id="1" src="img/events/progress_button.png">
-            <img onClick="show_event_decription(this.id);moveCar(this.id);" id="2" src="img/events/progress_button.png">
-            <img onClick="show_event_decription(this.id);moveCar(this.id);" id="3" src="img/events/progress_button.png">
-            <img onClick="show_event_decription(this.id);moveCar(this.id);" id="4" src="img/events/progress_button.png">
-            <img onClick="show_event_decription(this.id);moveCar(this.id);" id="5" src="img/events/progress_button.png">
-            <img onClick="show_event_decription(this.id);moveCar(this.id);" id="6" src="img/events/progress_button.png">
-            <img onClick="show_event_decription(this.id);moveCar(this.id);" id="7" src="img/events/progress_button.png">
-            <img onClick="show_event_decription(this.id);moveCar(this.id);" id="8" src="img/events/progress_button.png">
+            <span class="eventfirsthalf"><img onClick="show_event_decription(this.id);moveCar(this.id);" id="0" src="img/events/event_weeks_button.png"></span>
+            <span class="eventfirsthalf"><img onClick="show_event_decription(this.id);moveCar(this.id);" id="1" src="img/events/event_weeks_button.png"></span>
+            <span class="eventfirsthalf"><img onClick="show_event_decription(this.id);moveCar(this.id);" id="2" src="img/events/event_weeks_button.png"></span>
+            <span class="subeventfirsthalf"><img onClick="show_event_decription(this.id);moveCar(this.id);" id="3" src="img/events/event_subweeks_button.png"></span>
+            <span class="eventfirsthalf"><img onClick="show_event_decription(this.id);moveCar(this.id);" id="4" src="img/events/event_weeks_button.png"></span>
+            <span class="break"><img onClick="show_event_decription(this.id);moveCar(this.id);" id="5" src="img/events/event_break_button.png"></span>
+            <span class="subeventsecondhalf"><img onClick="show_event_decription(this.id);moveCar(this.id);" id="6" src="img/events/event_subweeks_button.png"></span>
+            <span class="eventsecondhalf"><img onClick="show_event_decription(this.id);moveCar(this.id);" id="7" src="img/events/event_weeks_button.png"></span>
+            <span class="subeventsecondhalf"><img onClick="show_event_decription(this.id);moveCar(this.id);" id="8" src="img/events/event_subweeks_button.png"></span>
+            <span class="eventsecondhalf"><img onClick="show_event_decription(this.id);moveCar(this.id);" id="9" src="img/events/event_weeks_button.png"></span>
+            <span class="subeventsecondhalf"><img onClick="show_event_decription(this.id);moveCar(this.id);" id="10" src="img/events/event_subweeks_button.png"></span>
+            <span class="eventsecondhalf"><img onClick="show_event_decription(this.id);moveCar(this.id);" id="11" src="img/events/event_weeks_button.png"></span>
+            <span class="subeventsecondhalf"><img onClick="show_event_decription(this.id);moveCar(this.id);" id="12" src="img/events/event_subweeks_button.png"></span>
+            <span class="eventsecondhalf"><img onClick="show_event_decription(this.id);moveCar(this.id);" id="13" src="img/events/event_weeks_button.png"></span>
           </div>
 
         <div class="events">
+          <!--Content of event 0-->
+          <h3 id="event_0" style="display:block;">O - Week</h3>
+          <h4 id="event_0_date" style="display:block;">26 February - 2 March</h4>
+          <p id="event_0_desc" style="display:block;">
+            Hey Guys!<br><br>
+Are you excited for Semester 1 2018 ? We ARE !<br><br>
+
+PPIA USYD would like to welcome all new students and returning students to USYD.<br><br>
+
+WE will be opening a stall at USYD on
+Wednesday 28 February until Friday 2 March
+9am - <br><br>
+
+It&#39;s in front of the Quadrangle, stalls no 265 B<br><br>
+
+New Students?
+If you are interested in joining PPIA (we hope you do!), there will be $10 joining fee. Please be advised that you must register to USU and get an access card prior to joining any clubs and societies in USYD.<br><br>
+
+<a href="https://www.usu.edu.au/Shop/ACCESS-Card/Access-Card.aspx" target="_blank">https://www.usu.edu.au/Shop/ACCESS-Card/Access-Card.aspx</a>
+Or
+Visit USU at Level 1 Manning House, Manning Road, Camperdown, NSW 2050.<br><br>
+
+Returning Students?
+If you are already a member of PPIA, please kindly redeem your new PPIA Card!<br><br>
+
+Stay Calm and Worry not if you cannot attend the O week! You can still sign up at any of our events. You can also contact Eileen, our dearest secretary, at 0 432 128 276.<br><br>
+
+Stay Tuned for our Facebook Group for our upcoming events !<br><br>
+          </p>
         <!--Content of event 1-->
-        <h3 id="event_1" style="display:block;">Ngerumpi x Lost in USyd (9-10 March 2018)</h3>
-        <p id="event_1_desc" style="display:block;">Description of event 1</p>
+        <h3 id="event_1">Ngerumpi x Lost in USyd</h3>
+        <h4 id="event_1_date">9-10 March 2018</h4>
+        <p id="event_1_desc">
+          Hey guys! How&#39;s everyone doing?<br><br>
+
+          Congrats on surviving first week of Uni, We know it&#39;s been one heck of a week!<br><br>
+
+          But you shall not worry, let&#39;s relax those brains up and have some fun!
+          What a great way to end this week by attending to our exciting events you shouldn&#39;t miss this semester, presented to you by the one and only, PPIA USYD.<br><br>
+
+          Don&#39;t wait up, because you are invited to our very first WELCOME GATHERING (Ngerumpi) that is held on 09 March 2018 at Old Teacher&#39;s College Rm 427.
+          Don&#39;t miss the fun y&#39;all!! Feel free to bring all your friends along with you and take this opportunity to socialize and mingle with other members of PPIA USYD. Also, FREE FOOD and GAMES awaits you!<br><br>
+
+          Next up on the list, LOST IN USYD vol. 2 is back this semester! ARE YOU GUYS EXCITED ?!! Shoutout to all Running Man fans, you guys definitely gonna love this one. Friends and games are the perfect combo, don&#39;t you guys agree? We&#39;ve prepared you variety of team building games that y&#39;all never imagine before. So just stay tuned!<br><br>
+
+          Meeting point: The Quadrangle
+          Date and Time: 10 March 2018 at 09:00 A.M<br><br>
+
+          Look forward to seeing all the newcomers and seniors this semester, I hope you guys are as thrilled as we are. See you guys there and Let&#39;s have some fun together!
+        </p>
         <!--Content of event 2-->
-        <h3 id="event_2">Beach BBQ (17 March 2018)</h3>
-        <p id="event_2_desc">Description of event 2</p>
-        <!--Content of event 3-->
-        <h3 id="event_3">AFF (29 March 2018)</h3>
-        <p id="event_3_desc">Description of event 3</p>
+        <h3 id="event_2">Beach BBQ</h3>
+        <h4 id="event_2_date">17 March 2018</h4>
+        <p id="event_2_desc">
+          Hey Guys! Hope ya&#39;ll had a great first week in uni. But get ready for our first ever get together of the year and we&#39;ll make sure we do it right.<br><br>
+
+It&#39;s summer here in the down under and what could be more perfect than a chill day out enjoying some BBQ and cracking a cold one with your friends overlooking the scenic beaches of Sydney?<br><br>
+
+Bring your sunnies, hats and spf because this week we&#39;re heading to the beach for a fun day out in the sun.
+There will be delicious food, games, sports and good music, it&#39;s the perfect get together to socialise, mingle and vibe with everyone!<br><br>
+
+Looking forward to having you there! Feel free to bring a plus one or even better your own crowd to come hang with all of us.<br><br>
+
+Meeting Point: TBA
+Date and Time: 17th March 2018 12.00 PM
+        </p>
+        <!--conectiontent of event 3-->
+        <h3 id="event_3">Badminton</h3>
+        <h4 id="event_3_date">TBC</h4>
+        <p id="event_3_desc">
+          Three weeks into uni and already buried under the countless lecture slides that takes up all your time to socialise and workout?<br><br>
+
+RISE AND SHINE!! It&#39;s time for you to wake up and forget all of your burden. Get ready to kill two birds with one stone! Make friends and stay fit with us every alternate Saturday through our fortnightly sports event.<br><br>
+
+This week we will be having a badminton friendly game at TBA (location and time). Be there and we guarantee you tons of fun through sweat!!!<br><br>
+
+Can&#39;t wait to see you guys there! Bring your friends, your families, your neighbors, your pets, everyone! The more the merrier!<br><br>
+
+PS: Do bring your own racquets OR contact us if you don&#39;t have one!<br><br>
+        </p>
         <!--Content of event 4-->
-        <h3 id="event_4">CAMP (Semester break)</h3>
-        <p id="event_4_desc">Description of event 4</p>
+        <h3 id="event_4">AFF</h3>
+        <h4 id="event_4_date">29 March 2018</h4>
+        <p id="event_4_desc">Asian Food Festival</p>
         <!--Content of event 5-->
-        <h3 id="event_5">MOCA & Circular Quay Trip (21 April 2018)</h3>
+        <h3 id="event_5">CAMP</h3>
+        <h4 id="event_5_date">Semester break</h4>
         <p id="event_5_desc">Description of event 5</p>
         <!--Content of event 6-->
-        <h3 id="event_6">Paintball (5 May 2018)</h3>
+        <h3 id="event_6">Sport</h3>
+        <h4 id="event_6_date">TBC</h4>
         <p id="event_6_desc">Description of event 6</p>
         <!--Content of event 7-->
-        <h3 id="event_7">Game Night (18 May 2018)</h3>
-        <p id="event_7_desc">Description of event 7</p>
+        <h3 id="event_7">MOCA & Circular Quay Trip</h3>
+        <h4 id="event_7_date">21 April 2018</h4>
+        <p id="event_7_desc">
+          Jalan-jalan with PPIA: MOCA & Circular Quay<br><br>
+
+          It&#39;s already half a semester in uni and I&#39;m guessing ya&#39;ll had a fair share of fisher library now and then, cause we know we do.<br><br>
+
+          But why not escape the same anxiety and stress inducing environment for a day and experience a more cultural and aesthetic surrounding. It&#39;s the perfect way to unwind, destress and find inspiration to motivate us through the rest of the semester.<br><br>
+
+          Come with us for a day out in Circular Quay and the Museum of Contemporary Art. We&#39;ll be having lunch, hanging out and looking at rad art just a few walks away from the Sydney Opera.<br><br>
+
+          Be sure to invite your friends and let&#39;s have a fun day together. See ya&#39;ll there!<br><br>
+
+          Meeting Point: Circular Quay Train Station
+          Date: 21st April 2018 12.00 PM
+        </p>
         <!--Content of event 8-->
-        <h3 id="event_8">End of Semester Dinner (2 June 2018))</h3>
+        <h3 id="event_8">Sport (TBC)</h3>
+        <h4 id="event_8_date">TBC</h4>
         <p id="event_8_desc">Description of event 8</p>
+        <!--Content of event 9-->
+        <h3 id="event_9">Paintball</h3>
+        <h4 id="event_9_date">5 May 2018</h4>
+        <p id="event_9_desc">
+          Call of Duty/ Battlefield/ Paintball Battle/ Counter Offensive<br><br>
+
+          Prepare your tactical skills guys and girls because we&#39;re going into battle! Invite your friends and team up and be ready to face off your enemies.<br><br>
+
+          It&#39;s going to be a thrilling<br><br>
+        </p>
+        <!--Content of event 10-->
+        <h3 id="event_10">Sport</h3>
+        <h4 id="event_10_date">TBC</h4>
+        <p id="event_10_desc">Description of event 10</p>
+        <!--Content of event 11-->
+        <h3 id="event_11">Game Night</h3>
+        <h4 id="event_11_date">18 May 2018</h4>
+        <p id="event_11_desc">
+          GAME NIGHT<br><br>
+
+          Up for some Pizza, FIFA and good ol&#39; board games?
+          Come through this friday to chill, hang out and play some games with us. There will be PS4, Monopoly, Taboo, and card games to play with all your friends.
+          Don&#39;t think your favorite video or board game is there? Feel free to bring yours!<br><br>
+
+          Invite your friends, your foes, anyone who&#39;s up for a fun and quick distraction from the stress of upcoming deadlines. And on top of that free pizza, snacks and drinks for all Access holders.<br><br>
+
+          Be sure to come hang and chill with us!<br><br>
+
+
+          Venue: TBA
+          Date: 18th May 2018  6.00 PM
+        </p>
+        <!--Content of event 12-->
+        <h3 id="event_12">Sport</h3>
+        <h4 id="event_12_date">TBC</h4>
+        <p id="event_12_desc">Description of event 12</p>
+        <!--Content of event 12-->
+        <h3 id="event_13">End of Semester Dinner</h3>
+        <h4 id="event_13_date">2 June 2018</h4>
+        <p id="event_13_desc">
+          Drink Dine Bowl<br><br>
+
+          Let&#39;s end this semester with our last (but not least) get together .
+          This time in the scenic nightlife of Darling Harbour where you can enjoy the city lights and beautiful waters of Sydney. Have the best italian dinner with us at the famous Criniti&#39;s then have a fun bowling game with all your favorite people.<br><br>
+
+          It&#39;s the best way to kiss the semester goodbye because you know and we know that you had pushed through and survived. So let loose and have a fun night out with us! We promise you it will be a night to remember.<br><br>
+
+          Meeting Point: TBA
+          Date: 2nd June 2018 6.00 PM
+        </p>
       </div>
       </div>
     </div>
